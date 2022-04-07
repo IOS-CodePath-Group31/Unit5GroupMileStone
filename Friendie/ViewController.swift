1a2946eb7264e4932434ee06ec4f73a625bacd99
Mimi Ogunyemi <98125679+mimiog@users.noreply.github.com>
2022-04-06 23:23:07 -0400

    uploaded files

---
+++ Friendie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friendie
+//
+//  Created by Mimi Ogunyemi on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
