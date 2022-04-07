777d076487ed4bc75927d932b1aace2be791810e
Mimi Ogunyemi <98125679+mimiog@users.noreply.github.com>
2022-04-07 00:43:50 -0400

    latest update
    The latest update implements the parse and sign-up page. I could not get the simulator to run on my Mac, but you can try. Build succeeds.

---
+++ Friendie/loginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  loginViewController.swift
+//  Friendie
+//
+//  Created by Mimi Ogunyemi on 4/6/22.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    
+    @IBOutlet weak var loginUsernameField: UITextField!
+    
+    
+    @IBOutlet weak var loginPasswordField: UITextField!
+    
+    @IBAction func onLogin(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Friendie/signUpViewController.swift
@@ -0,0 +1,54 @@
+//
+//  signUpViewController.swift
+//  Friendie
+//
+//  Created by Mimi Ogunyemi on 4/7/22.
+//
+
+import UIKit
+import Parse
+
+class signUpViewController: UIViewController {
+
+    @IBOutlet weak var signUpUsernameField: UITextField!
+    
+    
+    @IBOutlet weak var signUpPasswordField: UITextField!
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+          let user = PFUser()
+        user.username = signUpUsernameField.text
+        user.password = signUpPasswordField.text
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "signUpSegue", sender: nil)
+            }else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+        
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
