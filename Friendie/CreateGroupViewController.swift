0705bf8efd8c48da2439f3a71922206c5df25fd1
jabrayare <jabra5980@gmail.com>
2022-04-09 17:51:30 -0400

    User can create groups and can view a list of groups in the feed.

--- Friendie/CreateGroupViewController.swift
+++ Friendie/CreateGroupViewController.swift
@@ -6,24 +6,71 @@
 //
 
 import UIKit
+import AlamofireImage
+import Alamofire
+import Parse
 
-class CreateGroupViewController: UIViewController {
+class CreateGroupViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var groupName: UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onCreateGroup(_ sender: Any) {
+        let name = groupName?.text!
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        
+        let Group = PFObject(className: "Groups")
+        
+        Group["name"] = name
+        Group["image"] = file
+        Group["author"] = PFUser.current()
+        
+        Group.saveInBackground { (success, error) in
+            if success {
+                print("Saved")
+                self.dismiss(animated: true, completion: nil)
+            }
+            else {
+                print("Failed to create a Group!")
+            }
+        }
     }
-    */
+    
+    
+    @IBAction func onImageViewTap(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        }
+        else {
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af.imageAspectScaled(toFill: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+        
+        
+    }
 
 }
--- Friendie/feedViewController.swift
+++ Friendie/feedViewController.swift
@@ -10,6 +10,8 @@ import Parse
 
 class feedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet var groupTableView: UITableView!
+    
+    var groups = [PFObject]()
 
     
     @IBAction func onLogout(_ sender: Any) {
@@ -32,16 +34,45 @@ class feedViewController: UIViewController, UITableViewDelegate, UITableViewData
         groupTableView.delegate = self
         groupTableView.dataSource = self
         // Do any additional setup after loading the view.
+        loadGroups()
+    }
+    
+    func loadGroups() {
+        let query = PFQuery(className: "Groups")
+        query.includeKey("author")
+        query.limit = 5
+        self.groups.removeAll()
+        
+        query.findObjectsInBackground { (groups, error) in
+            if groups != nil {
+                self.groups = groups!
+                self.groupTableView.reloadData()
+                print("Groups fetched successfully!")
+                print(self.groups)
+            } else {
+                print("Failed fetching groups Data!")
+            }
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.groups.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = groupTableView.dequeueReusableCell(withIdentifier: "GroupTableViewCell", for: indexPath) as! GroupTableViewCell
-        cell.groupNameLabel?.text = "Best team ever!"
-        cell.createdByLabel?.text = "Jibril"
+        
+        let group = self.groups[indexPath.row]
+        
+        let author = group["author"] as! PFUser
+        let name = group["name"] as? String
+        let imageFile = group["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.groupNameLabel?.text = name
+        cell.createdByLabel?.text = author.username
+        cell.groupImageView.af.setImage(withURL: url)
         
         return cell
     }
