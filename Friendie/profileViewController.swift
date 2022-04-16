944bb87d34487b93fc3359cd988b004d8301997d
rogerliu37 <rliueml@gmail.com>
2022-04-15 22:58:07 -0400

    profile code

---
+++ Friendie/profileViewController.swift
@@ -0,0 +1,36 @@
+//
+//  profileViewController.swift
+//  Friendie
+//
+//  Created by Roger Liu on 4/15/22.
+//
+
+import UIKit
+import SwiftUI
+
+class profileViewController: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        profileImage.layer.borderWidth = 1
+        profileImage.layer.masksToBounds = false
+        profileImage.layer.borderColor = UIColor.black.cgColor
+        profileImage.layer.cornerRadius = profileImage.frame.height/2
+        profileImage.clipsToBounds = true
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
