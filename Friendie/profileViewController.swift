2e3c6e8f71ad7659cff9019e50a0a2cfb8e7f6e8
rogerliu37 <rliueml@gmail.com>
2022-04-16 23:13:15 -0400

    profiles

--- Friendie/profileViewController.swift
+++ Friendie/profileViewController.swift
@@ -6,11 +6,16 @@
 //
 import UIKit
 import SwiftUI
+import AlamofireImage
+import Parse
 
-class profileViewController: UIViewController {
+class profileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
-    
+    var profiles = [PFObject]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,6 +25,36 @@ class profileViewController: UIViewController {
         profileImage.layer.borderColor = UIColor.black.cgColor
         profileImage.layer.cornerRadius = profileImage.frame.height/2
         profileImage.clipsToBounds = true
+        
+        let author = PFUser.current()
+        let query = PFQuery(className: "Profiles")
+        query.includeKey("author")
+        query.limit = 1
+        query.order(byDescending: "createdAt")
+        query.findObjectsInBackground { (profiles, error) in
+            if profiles != nil {
+                self.profiles = profiles!
+                let profile = self.profiles[0]
+                let author = profile["author"] as! PFUser
+                let description = profile["description"] as? String
+                let imageFile = profile["image"] as! PFFileObject
+                let urlString = imageFile.url!
+                let url = URL(string: urlString)!
+                
+                self.usernameLabel?.text = author.username
+                self.descriptionLabel?.text = description
+                self.profileImage.af.setImage(withURL: url)
+                
+                print("Profiles fetched successfully!")
+                print(self.profiles)
+            } else {
+                print("Failed fetching profiles!")
+            }
+        }
+
+//        let description = profile["description"] as? String
+//        let imageFile = profile["image"] as! PFFileObject
+        
     }
     
     /*
